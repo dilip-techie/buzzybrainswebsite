d6c178b13ccaec6e03ab06e68e38f6b7df6fb565
dilip-techie <129263531+dilip-techie@users.noreply.github.com>
2025-12-15 00:12:17 +0530

    fixing small changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Buzzy Brains Academy: A Premier IITJEE/NEET Coaching Institute",
-  description: "Join Buzzy Brains Academy for top-notch IITJEE and NEET coaching. Expert faculty, comprehensive study materials, and proven success strategies to help you achieve your dreams.",
+  title: "Buzzy Brains Academy: A Premier IIT-JEE/NEET Coaching Institute",
+  description: "Join Buzzy Brains Academy for top-notch IITJEE and NEET coaching. We also offer premium coaching experience for Foundation and Olompiad preparation. We have Expert faculty, comprehensive study materials, and proven success strategies to help you achieve your dreams.",
 };
 
 export default function RootLayout({
