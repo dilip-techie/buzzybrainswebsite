fec59ca35bfd6b1038a3d756b10527498211beef
dilip-techie <129263531+dilip-techie@users.noreply.github.com>
2025-12-19 11:09:21 +0530

    add favicon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "BuzzyBrains Academy: Mentorship by IIT/IIM Alumni | A Premier IIT-JEE/NEET Coaching Institute.",
   description: "Join BuzzyBrains Academy for top-notch IIT-JEE, NEET and Foundation Programs. We also offer premium coaching experience for Foundation and Olompiad preparation. We have Expert faculty, comprehensive study materials, and proven success strategies to help you achieve your dreams.",
+  icons: {
+    icon: "/images/favicon.png",
+    apple: "/images/favicon.png",
+  },
   openGraph: {
     title: "BuzzyBrains Academy | Premium Coaching by IITian Mentor",
     description: "Experience premium academic mentorship led by IITian (IIT Kanpur) & IIM Ahmedabad alumni. Expert coaching for IIT-JEE, NEET, and Board exams.",
