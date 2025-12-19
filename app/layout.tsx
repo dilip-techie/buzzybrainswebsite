8ba4748349aac72c09ac869db7448bece9adb8fb
dilip-techie <129263531+dilip-techie@users.noreply.github.com>
2025-12-19 10:02:01 +0530

    added image tag.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,20 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "BuzzyBrains Academy: Mentorship by IIT/IIM Alumni | A Premier IIT-JEE/NEET Coaching Institute.",
   description: "Join BuzzyBrains Academy for top-notch IIT-JEE, NEET and Foundation Programs. We also offer premium coaching experience for Foundation and Olompiad preparation. We have Expert faculty, comprehensive study materials, and proven success strategies to help you achieve your dreams.",
+  openGraph: {
+    title: "BuzzyBrains Academy | Premium Coaching by IITian Mentor",
+    description: "Experience premium academic mentorship led by IITian (IIT Kanpur) & IIM Ahmedabad alumni. Expert coaching for IIT-JEE, NEET, and Board exams.",
+    url: "https://buzzybrainsacademy.com",
+    type: "website",
+    images: [
+      {
+        url: "/images/buzzybrains_social.jpg",
+        width: 1200,
+        height: 630,
+        alt: "BuzzyBrains Academy - Premium Coaching by IITian Mentor",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
